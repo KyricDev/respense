2adf530de288c64f5a708ab5656898c57b35e602
KyricDev <madridrickydave@gmail.com>
2022-01-03 08:02:15 +0800

    Added Register Form
    -Added Register Form on Landing Page. Can be accessed via button "To
    Register"

--- src/public/js/index.tsx
+++ src/public/js/index.tsx
@@ -1,21 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { any } from 'sequelize/dist/lib/operators';
 
-class Root extends React.Component{
+class Root extends React.Component<any, any>{
+    constructor(props: any){
+        super(props);
+        this.state = {
+            isOnLogin: true
+        }
+        this.changeForm = this.changeForm.bind(this);
+    }
+    changeForm(){
+        let currentState = this.state.isOnLogin;
+        this.setState({ isOnLogin: !currentState});
+    }
     render(){
+        let formAction = "/login";
+        let formState = "Login";
+        let formButton = "to register";
+        let confirmPassword = null;
+        let spacing = null;
+        if (!this.state.isOnLogin){
+            formAction = "form method=\"post\" action=\"/register\"";
+            formState = "Register";
+            formButton = "to login";
+            confirmPassword = <input type="password" id="confirmPassword" name="confirmPassword" placeholder="confirm password" autoComplete="password"></input>;
+            spacing = <br />;
+        }
         return(
             <div>
-                <div>Login your Account</div>
+                <div>{formState} your Account</div>
                 <br />
-                <form method="post" action="/login">
-                    <input type="text" id="username" name="username" placeholder="username"></input>
+                <button type="button" onClick={this.changeForm}>{formButton}</button>
+                <br />
+                <br />
+                <form method="post" action={formAction}>
+                    <input type="text" id="username" name="username" placeholder="username" autoComplete="username"></input>
                     <br />
-                    <input type="password" id="username" name="password" placeholder="password"></input>
+                    <input type="password" id="password" name="password" placeholder="password" autoComplete="password"></input>
                     <br />
+                    {confirmPassword}
+                    {spacing}
                     <br />
-                    <input type="submit" value="login"></input>
-                    <br />
-                    <button type="button">login</button>
+                    <input type="submit" value={formState.toLowerCase()}></input>
                 </form>
             </div>
         )
